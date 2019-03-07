f37153ad0f315c3bea9b886bfc3a1191c0c5a8c3
Albert Lacarta <albert.lacarta@lyra-network.com>
2019-03-07 14:08:12 +0100

    Adds integration with Selenium tests/playground on local playground

--- frameworks/vue/integration/src/App.vue
+++ frameworks/vue/integration/src/App.vue
@@ -6,15 +6,33 @@
 
 <script>
 import KRGlue from "@lyracom/embedded-form-glue";
+import _ from "underscore";
 
-let formToken = "01T41F_ggiSHm5Z772PiISZQ192eyJhIjoxMDAwMCwibSI6IkVVUiIsIm8iOm51bGwsImMiOnsiYiI6eyJ2aSI6eyJmIjp7InZhZENhcmRUeXBlIjp7InZhbHVlIjoiVklTQSJ9fX0sIm1jIjp7ImYiOnsidmFkQ2FyZFR5cGUiOnsidmFsdWUiOiJNQVNURVJDQVJEIn19fSwiYW0iOnsiZiI6eyJ2YWRDYXJkVHlwZSI6eyJ2YWx1ZSI6IkFNRVgifX19LCJjYiI6eyJmIjp7ImRlYml0Q3JlZGl0Ijp7InZhbHVlIjoiY3JlZGl0In19fX19LCJzaG9wTmFtZSI6IlNvIGVhc3kgdG8gdGVzdCIsImZvcm1BY3Rpb24iOiJQQVlNRU5UIn06002";
+const getQuery = function() {
+    let queryString = window.location.href.split("?")[1];
+    let query = {};
+
+    let segments = queryString.split("&");
+    _.each(segments, segment => {
+        let splittedSegment = segment.split("=");
+        query[splittedSegment[0]] = splittedSegment[1];
+    });
+
+
+    return query;
+};
+
+let query = getQuery();
+let formToken = query.formToken;
+let endpoint = query.endpoint;
+let publicKey = query.publicKey;
 
 export default {
   name: 'app',
   components: {
   },
   mounted: function() {
-      KRGlue.loadLibrary('http://krypton.local', '69876357:testpublickey_DEMOPUBLICKEY95me92597fd28tGD4r5').then(KR => {
+      KRGlue.loadLibrary(endpoint, publicKey).then(KR => {
           return KR.setFormConfig({
               formToken,
           });
@@ -26,6 +44,3 @@ export default {
   }
 }
 </script>
-
-<style>
-</style>
