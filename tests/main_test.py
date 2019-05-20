1e2e4248e740360b7fe4dbaa033f123257afe892
Sergi Yagües <s.yagues@lyra-network.es>
2019-05-20 16:53:09 +0200

    fix: Library reload
    Modified the library reload check to not
    cut the execution.
    
    Added a test to check if the library works
    as expected after the first load.

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+import time
 
 from helpers import TestMixins
 from selenium import webdriver
@@ -37,6 +38,13 @@ class MainTestCase(unittest.TestCase, TestMixins):
         self.selenium.get("{0}/tests/views/attachFormMinimal.html".format(os.getenv("WEB_HOST")))
         self.selenium.find_element_by_id("test-ready")
         self.check_button_label("PAY ME NOW")
+
+    # python tests/main_test.py MainTestCase.test_add_form_reload
+    def test_attach_form_minimal(self):
+        self.selenium.get("{0}/tests/views/addFormReload.html".format(os.getenv("WEB_HOST")))
+        time.sleep(5)
+        self.selenium.find_element_by_id("test-ready")
+        self.check_button_label("PAY")
 
     def tearDown(self):
         super().setUp()
