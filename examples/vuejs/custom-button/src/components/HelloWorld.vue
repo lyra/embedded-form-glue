740767c8f58432239b40adc82f2e0af4faff6a21
Albert Lacarta <albert.lacarta@lyra-network.com>
2019-06-26 14:46:44 +0200

    feature: Custom button payment trigger
    Adds sample on custom-button for payment trigger avoiding use the
    kr-payment-button generated by KR
    
    See: KRJS-146

--- examples/vuejs/custom-button/src/components/HelloWorld.vue
+++ examples/vuejs/custom-button/src/components/HelloWorld.vue
@@ -2,7 +2,9 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
       <div class="container">
-        <div id="myPaymentForm"></div>
+        <div id="myPaymentForm">
+          <button @click="callPay()">Custom trigger button</button>
+        </div>
       </div>
   </div>
 </template>
@@ -14,9 +16,15 @@ import KRGlue from "@lyracom/embedded-form-glue";
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+  },
+  data: function() {
+    return {
+      formId: null,
+    }
   },
   mounted() {
+    const _this = this
     /* define the public key, you should use your personal key */
     const publicKey = '69876357:testpublickey_DEMOPUBLICKEY95me92597fd28tGD4r5';
 
@@ -25,18 +33,31 @@ export default {
         formToken: 'DEMO-TOKEN-TO-BE-REPLACED',
         'kr-language': 'en-US',                           /* to update initialization parameter */
       }))
-      .then(({KR}) => KR.addForm('#myPaymentForm'))             /* create a payment form */
-      .then(({KR, result}) => KR.showForm(result.formId));      /* show the payment form */
+      .then(({KR}) => KR.attachForm('#myPaymentForm'))             /* create a payment form */
+      .then(({KR, result}) => {
+        // Show the payment form
+        KR.showForm(result.formId)
+        _this.formId = result.formId
+      });
+  },
+  methods: {
+    callPay: function() {
+      KR.submit(this.formId)
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
 .container {
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
+}
+
+.kr-payment-button {
+  display: none!important;
 }
 </style>
