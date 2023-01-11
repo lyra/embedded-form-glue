7eb85c098def6af6ed58ccebd75fa3c024ea8f6f
inavarro <iker.navarro@lyra-network.com>
2023-01-11 14:34:33 +0100

    chore(file-struct): Change examples file structure KJS-2488

---
+++ examples/vue/composition/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <add-form msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import AddForm from './components/AddForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AddForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
