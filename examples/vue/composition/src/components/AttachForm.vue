f38871dd7d07d8ee44aaf9ff6c443111a62c8c57
inavarro <iker.navarro@lyra-network.com>
2023-01-11 14:34:33 +0100

    chore(file-struct): Change examples file structure KJS-2488

---
+++ examples/vue/composition/src/components/AttachForm.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div class="container">
+      <div id="myPaymentForm">
+        <div class="kr-embedded">
+          <div class="kr-pan"></div>
+          <div class="kr-expiry"></div>
+          <div class="kr-security-code"></div>
+          <div class="kr-form-error"></div>
+          <button class="kr-payment-button"></button>
+        </div>
+      </div>
+    </div>
+    <div data-test="payment-message">{{ message }}</div>
+  </div>
+</template>
+
+<script>
+/* import embedded-form-glue library */
+import KRGlue from '@lyracom/embedded-form-glue'
+
+export default {
+  name: 'AttachForm',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      message: ''
+    }
+  },
+  mounted() {
+    const endpoint = '~~CHANGE_ME_ENDPOINT~~'
+    const publicKey = '~~CHANGE_ME_PUBLIC_KEY~~'
+    let formToken = 'DEMO-TOKEN-TO-BE-REPLACED'
+
+    // Generate the form token
+    
+    fetch('http://localhost:3000/createPayment', {
+      method: "POST",
+      body: {
+        paymentConf: { amount: 10000, currency: 'USD' }
+      }
+    })
+    .then(res => res.json())
+    .then(resp => {
+      formToken = resp.data
+      return KRGlue.loadLibrary(
+        endpoint,
+        publicKey
+      ) /* Load the remote library */
+    })
+    .then(({ KR }) =>
+      KR.setFormConfig({
+        /* set the minimal configuration */
+        formToken: formToken,
+        'kr-language': 'en-US' /* to update initialization parameter */
+      })
+    )
+    .then(({ KR }) => KR.onSubmit(this.validatePayment)) // Custom payment callback
+    .then(({ KR }) =>
+      KR.attachForm('#myPaymentForm')
+    ) /* create a payment form */
+    .then(({ KR, result }) => {
+      KR.showForm(result.formId)
+      this.ready = true
+    }) /* show the payment form */
+    .catch(
+      error => (this.message = error + ' (see console for more details)')
+    )
+  },
+  methods: {
+    /* Validate the payment data */
+    validatePayment(paymentData) {
+      fetch('http://localhost:3000/validatePayment', {
+        method: "POST",
+        body: paymentData
+      })
+      .then(res => res.json)
+      .then(response => {
+        if (response.status === 200) this.message = 'Payment successful!'
+      })
+      return false // Return false to prevent the redirection
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+</style>
